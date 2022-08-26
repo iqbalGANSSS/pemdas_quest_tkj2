10076f0e40b622d6b53136c5e8d06731a8697287
IKBL WRLD <111479662+iqbalGANSSS@users.noreply.github.com>
2022-08-25 18:50:10 -0700

    Add files via upload

---
+++ python6-iqbal.py
@@ -0,0 +1,29 @@
+print("1. Segitiga")
+
+alas = int(input("Nilai alas: "))
+tinggi = int(input("Nilai Tinggi: "))
+luas_s = alas * tinggi /2
+
+print("Luas segitiga adalah", luas_s)
+
+print("\n2. Persegi panjang")
+
+panjang = int(input("Nilai panjang: "))
+lebar = int(input("Nilai lebar: "))
+luas_pp = panjang * lebar
+
+print("luas Persegi panjang adalah", luas_pp)
+
+print("\n3. Jajargenjang")
+
+alas = float(input("Nilai alas: "))
+tinggi = float(input("Nilai tinggi: "))
+Luas_jg = alas * tinggi
+
+print("Luas Jajargenjang adalah", Luas_jg)
+
+print("\n4. Lingkaran")
+
+r = float(input("Masukkan jari jari"))
+luas_L = 3.14 *  r * r
+print("Luas lingkaran adalah", luas_L)
